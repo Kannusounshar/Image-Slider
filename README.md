b6118448e0c3af235251adb64d27b0bc46bce876
kannusounshar <kannusoundhar1234@gmail.com>
2023-10-28 00:27:59 +0530

    project done

---
+++ README.md
@@ -0,0 +1,10 @@
+Image Slider:
+An image Slider or Image Carousel is an expedient way to show multiple images on a website.
+
+Buildwith:
+-HTML
+-CSS
+-JAVASCRIPT
+
+Solution Link: https://github.com/Kannusounshar/Colour-Flipper
+You can view the live project here: https://relaxed-heliotrope-0f2c96.netlify.app/
---
+++ scripts/script.js
@@ -0,0 +1,35 @@
+let slideIndex = 0;
+showSlides(); 
+
+function showSlides() {
+  let i;
+
+  /
+  let slides = document.getElementsByClassName("image-sliderfade");
+
+  
+  let dots = document.getElementsByClassName("dot");
+
+  for (i = 0; i < slides.length; i++) {
+    
+    slides[i].style.display = "none";
+  }
+
+  // increase by 1, Global variable
+  slideIndex++;
+
+  // check for boundary
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+
+  // Change image every 2 seconds
+  setTimeout(showSlides, 2000);
+}
